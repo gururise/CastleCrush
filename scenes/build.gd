4c61cbc06638c1784f26156bfe8c25a7b7681916
Gene R <gururise@gmail.com>
2022-11-12 19:07:36 -0800

    Fix shape randomization and make a few bug fixes

--- scenes/build.gd
+++ scenes/build.gd
@@ -6,7 +6,6 @@ onready var tilemap : TileMap = $TileMap
 
 var shapes = ['I','SQUARE','L1', 'L2', 'S1', 'S2', 'T']
 var textures = {'I': preload("res://gfx/I.png"), 'SQUARE':  preload("res://gfx/SQUARE.png"), 'L1': preload("res://gfx/L1.png"), 'L2': preload("res://gfx/L2.png"),'S1': preload("res://gfx/S1.png"), 'S2': preload("res://gfx/S2.png"),'T': preload("res://gfx/T.png")}
-var shapes_full = []
 
 onready var block_sprite = get_node("Block")
 
@@ -16,8 +15,8 @@ var current_shape = shapes[0]
 func _ready():
 	randomize()
 	cells = tilemap.get_used_cells()
-	shapes_full = shapes.duplicate()
-	
+	current_shape = shapes[randi() % len(shapes)]
+	block_sprite.set_texture(textures[current_shape])
 	
 
 
@@ -30,7 +29,7 @@ func _input(event):
 		var local_position = tilemap.to_local(event.position)
 		var map_position = tilemap.world_to_map(local_position)
 		#var cell = tilemap.get_cellv(map_position)
-		if event.button_index == BUTTON_LEFT:
+		if event.button_index == BUTTON_LEFT and event.pressed == true:
 			match current_shape:
 				'I':
 					var current_x = map_position.x
@@ -38,8 +37,11 @@ func _input(event):
 					var I_tiles = [Vector2(current_x, current_y - 2), Vector2(current_x, current_y - 1), Vector2(current_x, current_y), Vector2(current_x, current_y + 1), Vector2(current_x, current_y + 2)]
 					for tile in I_tiles:
 						tilemap.set_cellv(tile, 1)
-					
-		else:
+			# select a new shape after placing the current shape
+			current_shape = _get_shape()
+			print(current_shape)
+			block_sprite.set_texture(textures[current_shape])
+		elif event.button_index == BUTTON_RIGHT and event.pressed == true:
 			tilemap.set_cellv(map_position, 0)
 	if event is InputEventKey and event.pressed:
 		if event.scancode == KEY_SPACE:
@@ -47,18 +49,11 @@ func _input(event):
 			# map position 
 			# map_position.x and map_position.y depending on shape
 			# if else on shape type is what you set_cell
-			current_shape = getShape()
+			current_shape = _get_shape()
 			block_sprite.set_texture(textures[current_shape])
 
 	if event is InputEventMouseMotion:
 		block_sprite.position = event.position
 
-
-func getShape():
-	if shapes.empty():
-		shapes = shapes_full.duplicate()
-		shapes.shuffle()
-	var random_shape = shapes.pop_front()
-	
-	return random_shape
-
+func _get_shape():
+	return shapes[randi() % len(shapes)]

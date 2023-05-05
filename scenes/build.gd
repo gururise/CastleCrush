518d45685f5cf81706e20950ae9f033612f9002d
Minh Vu <minh@supergroup.co>
2023-05-05 09:34:13 -0700

    update shape textures and names

--- scenes/build.gd
+++ scenes/build.gd
@@ -4,8 +4,8 @@ extends Node2D
 var cells : Array
 onready var tilemap : TileMap = $TileMap
 
-var shapes = ['I','SQUARE','L1', 'L2', 'S1', 'S2', 'T']
-var textures = {'I': preload("res://gfx/I.png"), 'SQUARE':  preload("res://gfx/SQUARE.png"), 'L1': preload("res://gfx/L1.png"), 'L2': preload("res://gfx/L2.png"),'S1': preload("res://gfx/S1.png"), 'S2': preload("res://gfx/S2.png"),'T': preload("res://gfx/T.png")}
+var shapes = ["I","O","L", "J", "S", "Z", "T"]
+var textures = {"I": preload("res://gfx/I.png"), "O":  preload("res://gfx/O.png"), "L": preload("res://gfx/L.png"), "J": preload("res://gfx/J.png"),'S': preload("res://gfx/S.png"), 'Z': preload("res://gfx/Z.png"),'T': preload("res://gfx/T.png")}
 
 onready var block_sprite = get_node("Block")
 
@@ -15,45 +15,54 @@ var current_shape = shapes[0]
 func _ready():
 	randomize()
 	cells = tilemap.get_used_cells()
-	current_shape = shapes[randi() % len(shapes)]
+	# current_shape = shapes[randi() % len(shapes)]
+	current_shape = shapes[2]
 	block_sprite.set_texture(textures[current_shape])
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
 func _input(event):
+	var local_position = tilemap.to_local(event.position)
+	var map_position = tilemap.world_to_map(local_position)
+	if event is InputEventMouseMotion:
+		block_sprite.position = event.position - block_sprite
+		
 	if event is InputEventMouseButton:
-		var local_position = tilemap.to_local(event.position)
-		var map_position = tilemap.world_to_map(local_position)
-		#var cell = tilemap.get_cellv(map_position)
-		if event.button_index == BUTTON_LEFT and event.pressed == true:
-			match current_shape:
-				'I':
-					var current_x = map_position.x
-					var current_y = map_position.y
-					var I_tiles = [Vector2(current_x, current_y - 2), Vector2(current_x, current_y - 1), Vector2(current_x, current_y), Vector2(current_x, current_y + 1), Vector2(current_x, current_y + 2)]
-					for tile in I_tiles:
-						tilemap.set_cellv(tile, 1)
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			#var cell = tilemap.get_cellv(map_position)
+			var set_tiles = get_block_tiles(map_position, current_shape)
+			
+			for tile in set_tiles:
+				tilemap.set_cellv(tile, 1)
 			# select a new shape after placing the current shape
-			current_shape = _get_shape()
+			var prev_shape = current_shape
+			while prev_shape == current_shape:
+				current_shape = _get_shape()
+
 			print(current_shape)
-			block_sprite.set_texture(textures[current_shape])
-		elif event.button_index == BUTTON_RIGHT and event.pressed == true:
+			block_sprite.set_texture(textures[current_shape]) 
+				
+		if event.button_index == BUTTON_RIGHT and event.pressed == true:
 			tilemap.set_cellv(map_position, 0)
 	if event is InputEventKey and event.pressed:
 		if event.scancode == KEY_SPACE:
-			# change texture
-			# map position 
-			# map_position.x and map_position.y depending on shape
-			# if else on shape type is what you set_cell
-			current_shape = _get_shape()
+			var prev_shape = current_shape
+			while prev_shape == current_shape:
+				current_shape = _get_shape()
 			block_sprite.set_texture(textures[current_shape])
 
-	if event is InputEventMouseMotion:
-		block_sprite.position = event.position
 
+func get_block_tiles(map_position, shape):
+	var current_x = map_position.x
+	var current_y = map_position.y
+	match shape:
+		"I":
+			return [Vector2(current_x, current_y - 2), Vector2(current_x, current_y - 1), Vector2(current_x, current_y), Vector2(current_x, current_y + 1), Vector2(current_x, current_y + 2)]
+		"O":
+			return [Vector2(current_x, current_y), Vector2(current_x, current_y + 1), Vector2(current_x + 1, current_y), Vector2(current_x + 1, current_y + 1)]
+		"L":
+			return [Vector2(current_x, current_y), Vector2(current_x, current_y + 1), Vector2(current_x, current_y + 2), Vector2(current_x + 1, current_y + 2)]
 func _get_shape():
 	return shapes[randi() % len(shapes)]
